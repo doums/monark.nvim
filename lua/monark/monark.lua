8592edc8bb5f729848e59c4ff542388414c58415
pierre <dommerc.pierre@gmail.com>
2022-11-15 00:09:34 +0100

    fix: bug delete extmark on invalid buffer

--- lua/monark/monark.lua
+++ lua/monark/monark.lua
@@ -45,7 +45,9 @@ local function defer_clear(timeout, buffer)
     timeout,
     0,
     vim.schedule_wrap(function()
-      api.nvim_buf_del_extmark(buffer, ns_id, extmark_id)
+      if api.nvim_buf_is_valid(buffer) then
+        api.nvim_buf_del_extmark(buffer, ns_id, extmark_id)
+      end
     end)
   )
 end
