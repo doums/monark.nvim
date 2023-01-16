cfa61db9aa0631df735be106bb8f00a4c81ac844
dharmx <dharmx.dev@gmail.com>
2023-01-16 09:40:51 +0530

    fix: get_mode_render returns nil on yank and delete

--- lua/monark/monark.lua
+++ lua/monark/monark.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: param-type-mismatch
 --[[ This Source Code Form is subject to the terms of the Mozilla Public
 License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at https://mozilla.org/MPL/2.0/. ]]
@@ -124,6 +125,9 @@ function M.init(config)
         return
       end
       local data = get_mode_render(config.modes, mode)
+      if not data then
+        return
+      end
       _offset = data.offset or config.offset
       _hl_mode = data.hl_mode or config.hl_mode
       set_extmark({
